77db6f1ab53ad9b817e5add4a187d7a8ece2bf08
jasmaa <jasonmaa3955@gmail.com>
2024-05-05 00:18:14 -0400

    Add CodeBlock tests

---
+++ src/components/CodeBlock/CodeBlock.spec.tsx
@@ -0,0 +1,31 @@
+import { describe, expect, it } from "vitest";
+import { render } from "@testing-library/react";
+import createWrapper from "@cloudscape-design/components/test-utils/dom";
+import CodeBlock from ".";
+
+describe('test CodeBlock', () => {
+  it('renders', () => {
+    const { container, queryByText } = render(
+      <CodeBlock
+        code={`{"key": "value"}`}
+        language="json"
+      />
+    );
+
+    expect(container).not.toBeNull();
+  });
+
+  it('renders copy to clipboard when copy is enabled', () => {
+    const { container } = render(
+      <CodeBlock
+        code={`{"key": "value"}`}
+        language="json"
+        copyEnabled
+      />
+    );
+
+    const wrapper = createWrapper(container);
+
+    expect(wrapper.findCopyToClipboard()).toBeTruthy();
+  });
+})
